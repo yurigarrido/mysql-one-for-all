0242c620c0d10c998e5a23717907639e18f6d3e9
yurigarrido <y.garrido.yuri@gmail.com>
2021-11-02 00:40:51 -0300

    ajusta erro no nome da coluna no desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -3,5 +3,5 @@ SELECT
 CAST(MIN(P.valor_plano) AS decimal(10,2)) AS 'faturamento_minimo',
 MAX(P.valor_plano) AS 'faturamento_maximo',
 CAST(ROUND(SUM(P.valor_plano)) / 4 AS decimal(10,2)) AS 'faturamento_medio',
-ROUND(SUM(P.valor_plano),2) AS 'faturamento_maximo'
+ROUND(SUM(P.valor_plano),2) AS 'faturamento_total'
 FROM planos P;
